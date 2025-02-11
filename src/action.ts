7dc2dd607f2867ad372ec4f76fef6864711e71e9
ahaoboy <504595380@qq.com>
2025-02-12 00:00:14 +0800

    bump dep

--- src/action.ts
+++ src/action.ts
@@ -1,7 +1,8 @@
 import { addPath, getInput, setOutput } from '@actions/core'
 import { homedir } from 'os'
 import { join } from 'path'
-import { install } from '@easy-install/easy-install'
+import { addGithubPath, hasPath, isGithub } from 'crud-path'
+import { install, addExecutePermission } from '@easy-install/easy-install'
 
 install(
   {
@@ -11,11 +12,21 @@ install(
   },
   join(homedir(), '.easy-setup'),
 ).then((output) => {
-  for (const { installDir } of output) {
-    if (installDir) {
-      addPath(installDir)
-      console.log(`easy-setup: addPath`, installDir)
+  for (const files of Object.values(output)) {
+    for (const item of files) {
+      const { installDir } = item
+      if (installDir && !hasPath(installDir)) {
+        addPath(installDir)
+        if (isGithub()) {
+          addGithubPath(installDir)
+        }
+      }
     }
-    setOutput('install-dir', installDir)
+    if (files.length === 1 && files[0].installPath) {
+      const first = files[0].installPath
+      if (first) {
+        addExecutePermission(first)
+      }
+    }
   }
 })
