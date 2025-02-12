495d47f55a59616424bd31b0f96e348601142f3a
ahaoboy <504595380@qq.com>
2025-02-12 20:52:14 +0800

    ts: v1.3.3

--- src/action.ts
+++ src/action.ts
@@ -1,8 +1,7 @@
-import { addPath, getInput, setOutput } from '@actions/core'
+import { getInput, } from '@actions/core'
 import { homedir } from 'os'
 import { join } from 'path'
-import { addGithubPath, hasPath, isGithub } from 'crud-path'
-import { install, addExecutePermission } from '@easy-install/easy-install'
+import { install, addOutputToPath } from '@easy-install/easy-install'
 
 install(
   {
@@ -11,22 +10,4 @@ install(
     name: getInput('name'),
   },
   join(homedir(), '.easy-setup'),
-).then((output) => {
-  for (const files of Object.values(output)) {
-    for (const item of files) {
-      const { installDir } = item
-      if (installDir && !hasPath(installDir)) {
-        addPath(installDir)
-        if (isGithub()) {
-          addGithubPath(installDir)
-        }
-      }
-    }
-    if (files.length === 1 && files[0].installPath) {
-      const first = files[0].installPath
-      if (first) {
-        addExecutePermission(first)
-      }
-    }
-  }
-})
+).then(addOutputToPath)
