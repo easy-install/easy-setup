ef5e7fb7c0d5cf6a69920652b80eb13cf66ebe68
ahaoboy <504595380@qq.com>
2025-02-17 14:10:22 +0800

    bump dep

--- src/action.ts
+++ src/action.ts
@@ -1,4 +1,7 @@
-import { getMultilineInput } from '@actions/core'
+// FIXME: https://github.com/actions/toolkit/issues/1959
+// import { getMultilineInput } from '@actions/core'
+import { getMultilineInput } from './tool'
+
 import { addOutputToPath, install } from '@easy-install/easy-install'
 
 const urlList = getMultilineInput('url')
