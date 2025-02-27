2da86b6d80b7ae1ed987ce7ac4b75abebd879ac8
ahaoboy <504595380@qq.com>
2025-02-27 20:40:05 +0800

    bump dep

--- src/action.ts
+++ src/action.ts
@@ -1,25 +1,16 @@
 // FIXME: https://github.com/actions/toolkit/issues/1959
 // import { getMultilineInput } from '@actions/core'
 import { getMultilineInput } from './tool'
-
-import { addOutputToPath, install } from '@easy-install/easy-install'
+import { install } from '@easy-install/easy-install'
 
 const urlList = getMultilineInput('url')
-const versionList = getMultilineInput('version')
 const nameList = getMultilineInput('name')
 
 async function main() {
   for (let i = 0; i < urlList.length; i++) {
     const url = urlList[i]
-    const version = versionList[i]
     const name = nameList[i]
-    await install(
-      {
-        url,
-        version,
-        name,
-      },
-    ).then(addOutputToPath)
+    await install(url, name)
   }
 }
 
