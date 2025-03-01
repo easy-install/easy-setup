62fdce415d5a3e17f1a5dde94caae174751512e8
ahaoboy <504595380@qq.com>
2025-03-01 21:58:48 +0800

    v1.4.0

--- src/action.ts
+++ src/action.ts
@@ -2,11 +2,13 @@
 // import { getMultilineInput } from '@actions/core'
 import { getMultilineInput } from './tool'
 import { install } from '@easy-install/easy-install'
+import { version } from '../package.json'
 
 const urlList = getMultilineInput('url')
 const nameList = getMultilineInput('name')
 
 async function main() {
+  console.log(`easy-setup: ${version}`)
   for (let i = 0; i < urlList.length; i++) {
     const url = urlList[i]
     const name = nameList[i]
